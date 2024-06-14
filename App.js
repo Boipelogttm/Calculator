5d1031d84a38515dee3802bded61741ac6ca654e
Boipelogttm <136469840+Boipelogttm@users.noreply.github.com>
2024-06-14 13:24:11 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './Components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>KALKULATOR</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
